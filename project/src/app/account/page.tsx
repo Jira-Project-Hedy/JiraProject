6336d4d63dcf303d5d25e3430951bf4537da75e1
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-27 19:59:33 -0300

    account view

---
+++ project/src/app/account/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useState } from "react";
+import Login from "./login/page";
+import { LOGIN_VIEW } from "@/interfaces/enums";
+
+export default function Account(){
+    const [currentView, setCurrentView] = useState(LOGIN_VIEW.SIGN_IN)
+
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            {currentView === LOGIN_VIEW.SIGN_IN ? (
+                <Login setCurrentView={setCurrentView} />
+            ) : (
+                <div>Register</div>
+            )}
+        </div>
+    )
+}
